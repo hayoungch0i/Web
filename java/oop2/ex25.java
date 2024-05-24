5f382ee7d01760aa5a361c5d9a67e0f41ddda488
hayoungch0i <chy0930@outlook.kr>
2024-05-24 17:18:50 +0900

    2024.05.24

--- java/oop2/ex25.java
+++ java/oop2/ex25.java
@@ -1,5 +1,9 @@
 package oop2;
-
+/*
+ 상품에 대한 오더별 구매수량 및 1EA 비용을 산출한 CSV파일입니다.
+ 각 오더번호 별로 구매수량 * 1EA 비용에 대한 매출액을 배열로 생성하고, 총 합계 매출비용을 출력하는 프로세서를 구축하시오
+ 
+ */
 public class ex25 {
 
 	public static void main(String[] args) {
